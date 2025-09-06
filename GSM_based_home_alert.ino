d83d58118f64fb22647b88d4d0c1c4d3823c1478
RoshmithaRajesh <roshmitharajesh@gmail.com>
2025-09-06 16:45:59 +0530

    Second commit

--- GSM_based_home_alert.ino
+++ GSM_based_home_alert.ino
@@ -1,36 +1,39 @@
-int LED1=12;
-int GND1=13;
-int LED2=8;
-int GND2=9;
-int pirOutput=5;
-void setup()
-{
+
+int statusLED = 12;
+int alarmLED = 8;
+int pirSensorPin = 5;
+
+int Gnd1 = 13;
+int Gnd2 = 9;
+
+void setup() {
   Serial.begin(9600);
-  pinMode(LED1,OUTPUT);
-  pinMode(GND1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(GND2,OUTPUT);
-  pinMode(pirOutput,INPUT);
- 
-  digitalWrite(pirOutput,LOW);
-  digitalWrite(GND1,LOW);
-  digitalWrite(GND2,LOW);
-  digitalWrite(LED1,LOW);
-  digitalWrite(LED2,LOW);
+
+  pinMode(statusLED, OUTPUT);
+  pinMode(alarmLED, OUTPUT);
+  pinMode(Gnd1, OUTPUT);
+  pinMode(Gnd2, OUTPUT);
+  pinMode(pirSensorPin, INPUT);
+
+  digitalWrite(Gnd1, LOW);
+  digitalWrite(Gnd2, LOW);
+  digitalWrite(statusLED, LOW);
+  digitalWrite(alarmLED, LOW);
+
   delay(15000);
-  digitalWrite(LED1,HIGH);
+  digitalWrite(statusLED, HIGH);   // System ready indicator
 }
-void loop()
-{
-  if(digitalRead(pirOutput)==HIGH)
-  {
-    digitalWrite(LED2,HIGH);
-    Serial.println("OK");
-    delay(1000);
-    Serial.println("ATD+91xxxxxxxxxx;");
-    delay(15000);
+
+void loop() {
+  if (digitalRead(pirSensorPin) == HIGH) {
+    digitalWrite(alarmLED, HIGH);
+    Serial.println("Motion detected!");
+    
+    Serial.println("ATD+918075672633;");   // Replace with your number
+    delay(15000);   // Wait for call duration
+
     Serial.println("ATH");
-    digitalWrite(LED2,LOW);
+    digitalWrite(alarmLED, LOW);
     delay(1000);
   }
 }
